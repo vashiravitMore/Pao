800754467e257e3b2028039b63eaf050f5be8219
vashiravit.m <vashiravit.m@gmail.com>
2017-03-28 21:55:09 +0700

    triangle

--- start/routes/index.js
+++ start/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/triangle', function(req, res, next) {
+  res.render('triangle', { title: 'Express' });
+});
+
+
 module.exports = router;
